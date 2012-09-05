d6d468df35613903f991f5be5b20f4315efd4c0c
Adam Awan <adamja1@gmail.com>
2012-09-05 02:25:39 +0100

    Beginnings of models and logic

--- mailtasker/apps/mt/views.py
+++ mailtasker/apps/mt/views.py
@@ -1,27 +1,20 @@
 import logging
 
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 
-# Handler for HTTP POST to http://myhost.com/messages for the route defined above
+# Handler for HTTP POST to http://HOSTNAME/messages for the route defined above
 @csrf_exempt
 def incoming_message(request):
      if request.method == 'POST':
-         sender    = request.POST.get('sender')
-         recipient = request.POST.get('recipient')
-         subject   = request.POST.get('subject', '')
+        sender_email         = request.POST.get('sender')
+        sender, created      = User.objects.get_or_create(email=sender_email)
+        list_name            = request.POST.get('subject', '')
+        task_list, created   = TaskList.objects.get_or_create(
+                                            email=sender_email,
+                                            name=list_name,
+                                            )
+        body                  = request.POST.get('stripped-text', '')
 
-         body_plain = request.POST.get('body-plain', '')
-         body_without_quotes = request.POST.get('stripped-text', '')
-
-         logging.exception("Look up! ^^")
-         # note: other MIME headers are also posted here...
-
-         # attachments:
-         #for key in request.FILES:
-         #    file = request.FILES[key]
-             # do something with the file
-
-     # Returned text is ignored but HTTP status code matters:
-     # Mailgun wants to see 2xx, otherwise it will make another attempt in 5 minutes
      return HttpResponse('OK')
